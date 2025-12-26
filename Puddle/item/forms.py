f9a68e65e4ee782104a937b498de877395627092
pawloshabtez3 <pawloshabtez@gmail.com>
2025-12-26 16:48:17 +0300

    Initialize Django project "Puddle" with core and item apps, set up basic structure, and add essential templates and forms for user authentication and item management.

---
+++ Puddle/item/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Item
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'description', 'price', 'image',)
+
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+        }
+    
